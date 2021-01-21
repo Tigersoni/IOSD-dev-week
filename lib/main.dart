009cb314756f9ce8910ee69c620d9c9c90fa4c48
Tigersoni <77624904+Tigersoni@users.noreply.github.com>
2021-01-21 19:46:09 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     SystemChrome.setEnabledSystemUIOverlays([]);
     allPeople = [Person("Maanas", "Why so serious?")];
     allPeople.add(Person("Shubh", "Alright, Alright, Alright!"));
-//    allPeople.add(Person("Shubh", "Why so serious?"));
+//    allPeople.add(Person("Daanish", "Anime is alpha?"));
 
     return MaterialApp(
       home: Scaffold(
